a867faaac91805f1444e318faff6551d4eebca30
Bindu Yadav <binduyaduvanshi23@gmail.com>
2025-10-10 18:58:55 +0530

    Added icons

--- ecommerce/src/Pages/HomePage.jsx
+++ ecommerce/src/Pages/HomePage.jsx
@@ -7,6 +7,7 @@ export function HomePage() {
   return (
     <>
       <title>ecommerce</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header />
 
       <div className="home-page">
