593043b48081a1276d326b1253a361e56c66f9a6
Bindu Yadav <binduyaduvanshi23@gmail.com>
2025-10-11 11:25:46 +0530

    Fetch data from the backend

--- ecommerce/src/Pages/HomePage.jsx
+++ ecommerce/src/Pages/HomePage.jsx
@@ -5,6 +5,14 @@ import Checkmark from '../assets/images/icons/checkmark.png';
 import {products} from '../../starting-code/ecommerce-project-main/data/products.js';
 
 export function HomePage() {
+
+  fetch('http://localhost:3000/api/products')
+  .then((response)=>{
+    return response.json();
+  }).then((data)=>{
+    console.log(data);
+  });
+
   return (
     <>
       <title>ecommerce</title>
